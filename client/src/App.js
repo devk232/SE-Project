202ab1138454c76869381ec599e7d59eb6504c87
Sachin Toppa <algorithms2019@gmail.com>
2021-04-28 16:03:55 +0530

    fixed some code

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,6 @@ const App = () => {
   const [user, setUser] = useState({
     isAuthenticated: false,
   });
-<<<<<<< HEAD
   if(localStorage.length>0){
      const Start=async()=>{
        const jwt=localStorage.getItem("token");
@@ -53,35 +52,5 @@ const App = () => {
       </BrowserRouter>
     );
 }
-=======
-  useEffect(() => {
-    const Start = async () => {
-      const jwt = localStorage.getItem("token");
-      const user_jwt = jwtDecode(jwt);
-      console.log(user_jwt);
-      const USER = await http.get(`${APIEndPoint}/users/${user_jwt._id}`);
-      setUser({ user: USER, isAuthenticated: true });
-    };
-    Start();
-  }, []);
-  return (
-    <BrowserRouter>
-      <Fragment>
-        <Navbar user={user} />
-        <div className="container">
-          <Switch>
-            <Route path="/room" exact component={Home} />
-            <Route path="/room/:url" component={Video} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <Route path="/logout" component={LogOut} />
-            <Route path="/" component={MainPage} />
-          </Switch>
-        </div>
-      </Fragment>
-    </BrowserRouter>
-  );
-};
->>>>>>> 48d84ec051fafa4a995d6b1c18a5bad84e79e5c7
 
 export default App;
